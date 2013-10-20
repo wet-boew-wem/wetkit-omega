733c44da8547f8e068f230440f24b68a3bb7a004
Will <sylus1984@gmail.com>
2013-10-20 02:40:35 -0400

    Improve the link it modal css form.

--- theme-settings.php
+++ theme-settings.php
@@ -15,6 +15,6 @@ function wetkit_omega_form_system_theme_settings_alter(&$form, $form_state) {
   // settings form for your subtheme. You should also take a look at the
   // 'extensions' concept in the Omega base theme.
   if (isset($form['omega']['layouts'])) {
-    $form['omega']['layouts']['#prefix'] = '<div class="messages warning">' . t('This has been purposely disabled by Drupal WxT as we use a panels based layout system.') . '</div>';
+    $form['omega']['layouts']['#prefix'] = '<div class="messages warning wxt-message">' . t('This has been purposely disabled by Drupal WxT as we use a panels based layout system.') . '</div>';
   }
 }
