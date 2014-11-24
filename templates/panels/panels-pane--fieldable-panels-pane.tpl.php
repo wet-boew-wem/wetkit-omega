00615bf88f4deb1fd2ad8ca84ff98e5361d51936
William Hearn <sylus1984@gmail.com>
2014-11-24 14:58:30 -0500

    git commit -m 'Issue #2374829: panels-pane.tpl.php files out of date in wetkit-omega'

--- templates/panels/panels-pane--fieldable-panels-pane.tpl.php
+++ templates/panels/panels-pane--fieldable-panels-pane.tpl.php
@@ -25,7 +25,7 @@
 <?php if ($pane_prefix): ?>
   <?php print $pane_prefix; ?>
 <?php endif; ?>
-<div class="<?php print $classes; ?>" <?php print $id; ?>>
+<div class="<?php print $classes; ?>" <?php print $id; ?> <?php print $attributes; ?>>
   <div class="block-inner clearfix">
     <?php if ($admin_links): ?>
       <?php print $admin_links; ?>
